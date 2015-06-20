b9cad1c0912a6a3cdda3983eeb2926086f335ed8
Safin Roman <r.n.safin@gmail.com>
2015-06-20 11:18:28 +0300

    Creating the Front Controller

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
-//index.php
+// index.php
+
+// load and initialize any global libraries
 require_once 'model.php';
+require_once 'controllers.php';
 
-$posts = get_all_posts();
-
-require 'templates/list.php';
+// route the request internally
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+if ('/' == $uri) {
+    list_action();
+} elseif ('/show' == $uri && isset($_GET['id'])) {
+    show_action($_GET['id']);
+} else {
+    header('Status: 404 Not Found');
+    echo '<html><body><h1>Page Not Found</h1></body></html>';
+}
--- model.php
+++ model.php
@@ -26,3 +26,16 @@ function get_all_posts()
 
     return $posts;
 }
+function get_post_by_id($id)
+{
+    $link = open_database_connection();
+
+    $id = intval($id);
+    $query = 'SELECT date, title, body FROM post WHERE id = '.$id;
+    $result = mysql_query($query);
+    $row = mysql_fetch_assoc($result);
+
+    close_database_connection($link);
+
+    return $row;
+}
