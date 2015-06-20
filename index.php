c2dcb0b60c92f1eb14b8134558c5339174cc887e
Safin Roman <r.n.safin@gmail.com>
2015-06-20 11:04:47 +0300

    Isolating the Presentation

--- index.php
+++ index.php
@@ -4,27 +4,13 @@ $db = mysql_connect("localhost", "root", "");
 mysql_select_db("symfony_book", $db);
 
 $result = mysql_query('SELECT id, title FROM post', $db);
-?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>List of Posts</title>
-</head>
-<body>
-<h1>List of Posts</h1>
-<ul>
-    <?php while ($row = mysql_fetch_assoc($result)): ?>
-        <li>
-            <a href="/show.php?id=<?php echo $row['id'] ?>">
-                <?php echo $row['title'] ?>
-            </a>
-        </li>
-    <?php endwhile ?>
-</ul>
-</body>
-</html>
+$posts = array();
+while ($row = mysql_fetch_assoc($result)) {
+    $posts[] = $row;
+}
 
-<?php
 mysql_close($db);
-?>
+
+require 'templates/list.php';
+
